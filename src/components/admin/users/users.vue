7a50194eabe1aa1899926f858b0a3a0eeaf3ef29
Qianxiong Xu <20086454@mail.wit.ie>
2019-11-26 11:14:11 +0000

    Add vue-tables-2 and show the user list

--- src/components/admin/users/users.vue
+++ src/components/admin/users/users.vue
@@ -1,14 +1,56 @@
 <template>
   <div class="users">
     <h3 class="vue-title"><i class="fa fa-list" style="padding: 3px"></i>{{messageTitle}}</h3>
+    <div class="users-table">
+      <v-client-table :columns="columns" :data="users" :options="options"></v-client-table>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Service from '@/services/services'
+  import Vue from 'vue'
+  import VueTables from 'vue-tables-2'
+
+  Vue.use(VueTables.ClientTable, {compileTemplates: true, filterByColumn: true})
+
+  const ERR_OK = 0
+
   export default {
     data() {
       return {
-        messageTitle: 'User List'
+        messageTitle: 'User List',
+        users: [],
+        errors: [],
+        columns: ['_id', 'username', 'phone', 'address', 'pay', 'favorite'],
+        options: {
+          headings: {
+            _id: 'ID',
+            username: 'Username',
+            phone: 'Phone',
+            address: 'Address',
+            pay: 'Pay',
+            favorite: 'Favorite'
+          }
+        }
+      }
+    },
+    created () {
+      this._initializeUsers()
+    },
+    methods: {
+      _initializeUsers() {
+        Service.getUsers()
+          .then((response) => {
+            let res = response.data
+            if (res.code === ERR_OK) {
+              this.users = res.data
+              console.log(res.data)
+            }
+          })
+          .catch((err) => {
+            this.errors.push(err)
+          })
       }
     }
   }
@@ -20,4 +62,7 @@
     text-align: center
     font-size: 45pt
     margin-bottom: 10px
+  .users-table
+    width: 80%
+    margin: 0 auto
 </style>
