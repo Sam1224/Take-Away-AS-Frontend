15a48faa7374aabbcc40177476af043f47ec7f70
Qianxiong Xu <20086454@mail.wit.ie>
2019-12-05 13:29:18 +0000

    Add image uploader in addseller

--- src/components/admin/sellers/addseller.vue
+++ src/components/admin/sellers/addseller.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="add-wrapper">
+  <div class="add-wrapper" v-show="!loading">
     <h2 class="title">{{title}}</h2>
-    <el-form ref="sellerForm" :model="sellerForm" status-icon label-width="100px" class="seller-table" :rules="rules">
+    <el-form v-loading.fullscreen.lock="loading" element-loading-text="Loading..." element-loading-background="rgb(255, 255, 255)" ref="sellerForm" :model="sellerForm" status-icon label-width="100px" class="seller-table" :rules="rules">
       <el-form-item label="Name" prop="name">
         <el-input v-model="sellerForm.name" auto-complete="off"></el-input>
       </el-form-item>
@@ -15,20 +15,49 @@
         <el-input v-model="sellerForm.bulletin" auto-complete="off"></el-input>
       </el-form-item>
       <el-form-item label="Avatar" prop="avatar">
-        <el-input v-model="sellerForm.avatar" auto-complete="off"></el-input>
+        <el-upload ref="avatarUploader" class="upload-wrapper" drag action="https://takeawayapp-sam.herokuapp.com/upload" show-file-list
+                   :auto-upload="false"
+                   accept="image/png, image/jpeg"
+                   :on-progress="handleAvatarProgress"
+                   :disabled="avatarStatus"
+                   :file-list="avatarList"
+                   list-type="picture"
+                   :http-request="uploadAvatar"
+                   :limit="Number(1)"
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">Drag picture here, or <em>click to upload</em></div>
+          <div class="el-upload__tip" slot="tip">Only .jpg and .png files accepted</div>
+        </el-upload>
       </el-form-item>
       <el-form-item label="Pictures" prop="pics">
-        <ul>
-          <li class="pic-item" v-for="(pic, index) in sellerForm.pics" :key="index">
-            <div class="input-wrapper">
-              <el-input v-model="sellerForm.pics[index]" auto-complete="off"></el-input>
-            </div>
-            <div class="icon-wrapper">
-              <i class="iconBtn el-icon-remove-outline" @click="delPic(index)"></i>
-              <i class="iconBtn" :class="{'el-icon-circle-plus-outline': sellerForm.pics.length - 1 === index}" @click="addPic(index)"></i>
-            </div>
-          </li>
-        </ul>
+        <el-upload ref="picsUploader" class="upload-wrapper" drag action="https://takeawayapp-sam.herokuapp.com/uploadmul" show-file-list
+                   :auto-upload="false"
+                   accept="image/png, image/jpeg"
+                   :on-progress="handlePicsProgress"
+                   :file-list="picsList"
+                   list-type="picture"
+                   :disabled="picsStatus"
+                   :http-request="uploadPics"
+                   :limit="Number(10)"
+                   multiple
+        >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">Drag picture here, or <em>click to upload</em></div>
+          <div class="el-upload__tip" slot="tip">Only .jpg and .png files accepted, maximum: 10</div>
+        </el-upload>
+<!--        <ul>-->
+<!--          <li class="pic-item" v-for="(pic, index) in sellerForm.pics" :key="index">-->
+<!--            <div class="input-wrapper">-->
+<!--              <el-input v-model="sellerForm.pics[index]" auto-complete="off"></el-input>-->
+<!--            </div>-->
+<!--            <div class="icon-wrapper">-->
+<!--              <i class="iconBtn el-icon-remove-outline" @click="delPic(index)"></i>-->
+<!--              <i class="iconBtn" :class="{'el-icon-circle-plus-outline': sellerForm.pics.length - 1 === index}"-->
+<!--                 @click="addPic(index)"></i>-->
+<!--            </div>-->
+<!--          </li>-->
+<!--        </ul>-->
       </el-form-item>
       <el-form-item label="Supports" prop="supports">
         <ul>
@@ -36,7 +65,8 @@
             <div class="input-wrapper">
               <el-col :span="8">
                 <el-select id="cls-wrapper" v-model="sellerForm.supports[index].types" placeholder="Select Support">
-                  <el-option class="cls" v-for="(cls, idx) in classMap" :key="idx" :value="idx" :label="cls"></el-option>
+                  <el-option class="cls" v-for="(cls, idx) in classMap" :key="idx" :value="idx"
+                             :label="cls"></el-option>
                 </el-select>
               </el-col>
               <el-col :span="16">
@@ -45,7 +75,8 @@
             </div>
             <div class="icon-wrapper">
               <i class="iconBtn el-icon-remove-outline" @click="delSupport(index)"></i>
-              <i class="iconBtn" :class="{'el-icon-circle-plus-outline': sellerForm.supports.length - 1 === index}" @click="addSupport(index)"></i>
+              <i class="iconBtn" :class="{'el-icon-circle-plus-outline': sellerForm.supports.length - 1 === index}"
+                 @click="addSupport(index)"></i>
             </div>
           </li>
         </ul>
@@ -58,13 +89,14 @@
             </div>
             <div class="icon-wrapper">
               <i class="iconBtn el-icon-remove-outline" @click="delInfo(index)"></i>
-              <i class="iconBtn" :class="{'el-icon-circle-plus-outline': sellerForm.infos.length - 1 === index}" @click="addInfo(index)"></i>
+              <i class="iconBtn" :class="{'el-icon-circle-plus-outline': sellerForm.infos.length - 1 === index}"
+                 @click="addInfo(index)"></i>
             </div>
           </li>
         </ul>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="addSeller">Add Seller</el-button>
+        <el-button type="primary" @click="submitAvatar">Add Seller</el-button>
         <el-button @click="reset">Reset</el-button>
         <el-button @click="cancel">Cancel</el-button>
       </el-form-item>
@@ -74,15 +106,23 @@
 
 <script type="text/ecmascript-6">
   import Service from '@/services/services'
-  import { mapGetters } from 'vuex'
+  import {mapGetters} from 'vuex'
+  import axios from 'axios'
 
   const ERR_OK = 0
 
   export default {
-    data() {
+    data () {
       return {
         title: 'Add Seller',
         seller: {},
+        loading: false,
+        avatarStatus: false,
+        picsStatus: false,
+        avatarList: [],
+        picsList: [],
+        avatarFormData: '',
+        picsFormData: '',
         sellerForm: {
           name: '',
           description: '',
@@ -113,16 +153,11 @@
             required: true,
             message: 'Please enter bulletin',
             trigger: 'blur'
-          }],
-          avatar: [{
-            required: true,
-            message: 'Please enter avatar',
-            trigger: 'blur'
           }]
         }
       }
     },
-    created() {
+    created () {
       this.classMap = ['Decrease', 'Discount', 'Special', 'Invoice', 'Guarantee']
     },
     computed: {
@@ -131,7 +166,66 @@
       ])
     },
     methods: {
-      addSeller() {
+      handleAvatarProgress(event, file, fileList) {
+        this.avatarStatus = true
+      },
+      handlePicsProgress(event, file, fileList) {
+        this.picsStatus = true
+      },
+      uploadAvatar(file) {
+        this.avatarFormData.append('file', file.file)
+      },
+      uploadPics(file) {
+        this.picsFormData.append('files', file.file)
+      },
+      submitAvatar() {
+        this.loading = true
+        this.avatarFormData = new FormData()
+        this.$refs.avatarUploader.submit()
+        let config = {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }
+        axios.post('https://takeawayapp-sam.herokuapp.com/upload', this.avatarFormData, config)
+          .then((response) => {
+            this.sellerForm.avatar = response.data.filepath
+            this.avatarStatus = false
+            this.avatarList.push({
+              name: response.data.filepath.split('/')[-1],
+              url: `https://takeawayapp-sam.herokuapp.com/${response.data.filepath}`
+            })
+            this.submitPics()
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      },
+      submitPics() {
+        this.picsFormData = new FormData()
+        this.$refs.picsUploader.submit()
+        let config = {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        }
+        axios.post('https://takeawayapp-sam.herokuapp.com/uploadmul', this.picsFormData, config)
+          .then((response) => {
+            this.sellerForm.pics = response.data.filepaths
+            this.picsStatus = false
+            response.data.filepaths.forEach((filepath) => {
+              this.picsList.push({
+                name: filepath.split('/')[-1],
+                url: `https://takeawayapp-sam.herokuapp.com/${filepath}`
+              })
+            })
+            this.addSeller()
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      },
+      addSeller () {
         this.$refs.sellerForm.validate((valid) => {
           if (valid) {
             let seller = {
@@ -149,6 +243,7 @@
             Service.addSeller(seller)
               .then((response) => {
                 let res = response.data
+                this.loading = false
                 if (res.code === ERR_OK) {
                   this.$message({
                     showClose: true,
@@ -175,28 +270,28 @@
           }
         })
       },
-      reset() {
+      reset () {
         this.$refs.sellerForm.resetFields()
       },
-      cancel() {
+      cancel () {
         this.$router.push('/admin/sellers')
       },
-      addPic(index) {
+      addPic (index) {
         let newPic = ''
         this.sellerForm.pics.push(newPic)
       },
-      delPic(index) {
+      delPic (index) {
         if (this.sellerForm.pics.length === 1) {
           this.sellerForm.pics[0] = ''
           return
         }
         this.sellerForm.pics.splice(index, 1)
       },
-      addSupport(index) {
+      addSupport (index) {
         let newSupport = {types: '', description: ''}
         this.sellerForm.supports.push(newSupport)
       },
-      delSupport(index) {
+      delSupport (index) {
         if (this.sellerForm.supports.length === 1) {
           this.sellerForm.supports[0].types = ''
           this.sellerForm.supports[0].description = ''
@@ -204,11 +299,11 @@
         }
         this.sellerForm.supports.splice(index, 1)
       },
-      addInfo(index) {
+      addInfo (index) {
         let newInfo = ''
         this.sellerForm.infos.push(newInfo)
       },
-      delInfo(index) {
+      delInfo (index) {
         if (this.sellerForm.infos.length === 1) {
           this.sellerForm.infos[0] = ''
           return
@@ -226,22 +321,28 @@
     position: relative
     font-size: 0
     text-align: center
+
     .title
       font-size: 36px
       margin-top: 36px
+
     .seller-table
       position: relative
       width: 40%
       top: 50px
       margin: 0 auto
+
     .pic-item, .support-item, .info-item
       position: relative
       display: flex
+
       .input-wrapper
         flex: 0 0 90%
+
       .icon-wrapper
         flex: 1
         position: relative
+
         .iconBtn
           position: relative
           top: 15px
