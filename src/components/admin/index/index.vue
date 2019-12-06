9edabbf4fb83be25d738a399481f809605d89f01
Qianxiong Xu <20086454@mail.wit.ie>
2019-12-06 10:21:28 +0000

    Modify index page

--- src/components/admin/index/index.vue
+++ src/components/admin/index/index.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="index" v-loading.fullscreen.lock="loading" element-loading-text="Loading..." element-loading-background="rgb(255, 255, 255)">
     <div v-show="!loading" class="content-wrapper">
-      <h1 class="vue-title">Take-Away Backend</h1>
-
-      <table align="center">
-        <tr>
-          <td>
-            <p class="lead"><b><i>Time for a change !!</i></b></p>
-          </td>
-          <td>
-            <img src="../../../assets/homer.gif" alt="description here" />
-          </td>
-        </tr>
-      </table>
-
-      <p></p>
-      <p class="lead">This is the homepage of your <b>MEVN</b> Web app</p>
+      <h1 class="vue-title">Take-Away App - Backend</h1>
+      <el-carousel :interval="3000" arrow="hover" class="slides">
+        <el-carousel-item v-for="(food, index) in foods" :key="index">
+          <el-image :src="food" fit="fill"></el-image>
+        </el-carousel-item>
+      </el-carousel>
     </div>
   </div>
 </template>
@@ -24,7 +15,18 @@
   export default {
     data() {
       return {
-        loading: true
+        loading: true,
+        foods: [
+          require('../../../assets/food0.jpg'),
+          require('../../../assets/food1.jpg'),
+          require('../../../assets/food2.jpg'),
+          require('../../../assets/food3.jpg'),
+          require('../../../assets/food4.jpg'),
+          require('../../../assets/food5.jpg'),
+          require('../../../assets/food6.jpg'),
+          require('../../../assets/food7.jpg'),
+          require('../../../assets/food8.jpg')
+        ]
       }
     },
     created() {
@@ -42,21 +44,19 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
   .index
-    height: 100vh
-    margin-top: 30px
     align-items: center
     justify-content: center
     text-align: center
-    .vue-title
-      font-size: 70pt
-      margin-bottom: 10px
-    .lead
-      font-weight: 200
-      font-size: 2.5rem
-    .bg
-      position: absolute
-      width: 100%
-      height: 100%
-      z-index: 10
-      color: white
+    margin-top: 25px
+    .content-wrapper
+      text-align: center
+      width: 1280px
+      margin: 0 auto
+      .vue-title
+        font-size: 48px
+        margin-bottom: 25px
+      .slides
+        .el-carousel__container
+          width: 100%
+          height: 480px
 </style>
